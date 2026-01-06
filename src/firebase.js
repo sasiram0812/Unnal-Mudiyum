e0f158292bcaad5770905d2771d624ab5758a82d
Sasiram <sasiram0812@gmail.com>
2026-01-06 10:48:46 +0530

    ggg

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,18 @@
-// Import Firebase
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyBbcoNDYn6lXmwgtrzQkjjYKaLSVjwv7e0",
-  authDomain: "onal-mudiyum.firebaseapp.com",
-  projectId: "onal-mudiyum",
-  storageBucket: "onal-mudiyum.firebasestorage.app",
-  messagingSenderId: "843102660266",
-  appId: "1:843102660266:web:9efda8bafa88a13e7cd2d3",
-  measurementId: "G-KF34M4W6R0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase Authentication
 export const auth = getAuth(app);
-
-
-// Firestore Database (To-Do, Study, Fitness, Profile)
 export const db = getFirestore(app);
-
-export default app;
